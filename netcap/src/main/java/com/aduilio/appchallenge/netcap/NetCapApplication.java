4e35e13d675ffb5cd6e2251ba4440411326777fd
Aduilio Eduardo da Silva <aduilio@inatel.br>
2023-05-12 11:56:10 -0300

    Create the project.

---
+++ netcap/src/main/java/com/aduilio/appchallenge/netcap/NetCapApplication.java
@@ -0,0 +1,11 @@
+package com.aduilio.appchallenge.netcap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetCapApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(NetCapApplication.class, args);
+	}
+}
---
+++ netcap/src/test/java/com/aduilio/appchallenge/netcap/NetCapApplicationTests.java
@@ -0,0 +1,11 @@
+package com.aduilio.appchallenge.netcap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetCapApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
