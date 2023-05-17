8842dbe35689e4098f76e940888754da5bc0ea2f
Aduílio Silva <developer.aduilio@gmail.com>
2023-05-17 08:14:59 -0300

    Fix PDF file content
    - Create a data.sql file to populate the database with traffic for the previous months.

---
+++ netcap/src/main/resources/data.sql
@@ -0,0 +1,37 @@
+insert into traffics(pid, name, date, download, upload) values (12345, 'Netflix', '2023-05-14 10:00:00', 1000000000, 200000000);
+insert into traffics(pid, name, date, download, upload) values (34567, 'Chrome', '2023-05-12 10:00:00', 900000000, 20000000);
+
+insert into traffics(pid, name, date, download, upload) values (23456, 'Teams', '2023-05-01 10:00:00', 3330000000, 20000000);
+insert into traffics(pid, name, date, download, upload) values (34567, 'Chrome', '2023-05-02 10:00:00', 490000000, 20000000);
+
+insert into traffics(pid, name, date, download, upload) values (1234, 'Netflix', '2023-04-02 10:00:00', 6543240000, 200000000);
+insert into traffics(pid, name, date, download, upload) values (4556, 'Teams', '2023-04-03 10:00:00', 2234450000, 100000000);
+insert into traffics(pid, name, date, download, upload) values (5667, 'OneDrive', '2023-04-04 10:00:00', 443663000, 200000000);
+insert into traffics(pid, name, date, download, upload) values (6778, 'Whatsapp', '2023-04-05 10:00:00', 111222000, 100000000);
+insert into traffics(pid, name, date, download, upload) values (7889, 'Chrome', '2023-04-01 10:00:00', 2345670000, 100000000);
+
+insert into traffics(pid, name, date, download, upload) values (7665, 'Chrome', '2023-03-01 10:00:00', 2345670000, 100000000);
+insert into traffics(pid, name, date, download, upload) values (6554, 'Netflix', '2023-03-02 10:00:00', 6543240000, 200000000);
+insert into traffics(pid, name, date, download, upload) values (5443, 'Teams', '2023-04-03 10:00:00', 2234450000, 100000000);
+insert into traffics(pid, name, date, download, upload) values (4332, 'OneDrive', '2023-03-04 10:00:00', 443663000, 200000000);
+insert into traffics(pid, name, date, download, upload) values (55555, 'Whatsapp', '2023-03-05 10:00:00', 111222000, 100000000);
+
+insert into traffics(pid, name, date, download, upload) values (4123, 'Chrome', '2023-02-01 10:00:00', 664400000, 100000000);
+insert into traffics(pid, name, date, download, upload) values (5175, 'Teams', '2023-02-03 10:00:00', 4321000000, 100000000);
+insert into traffics(pid, name, date, download, upload) values (1976, 'OneDrive', '2023-02-04 10:00:00', 7450000000, 200000000);
+insert into traffics(pid, name, date, download, upload) values (3400, 'Whatsapp', '2023-02-05 10:00:00', 345609876, 100000000);
+
+insert into traffics(pid, name, date, download, upload) values (792423, 'Chrome', '2023-01-01 10:00:00', 4500000000, 100000000);
+insert into traffics(pid, name, date, download, upload) values (782434, 'Netflix', '2023-01-02 10:00:00', 9800000000, 200000000);
+insert into traffics(pid, name, date, download, upload) values (77353, 'Teams', '2023-01-03 10:00:00', 4700000000, 100000000);
+insert into traffics(pid, name, date, download, upload) values (752323, 'Whatsapp', '2023-01-05 10:00:00', 123456098, 100000000);
+
+insert into traffics(pid, name, date, download, upload) values (85433, 'Chrome', '2022-12-01 10:00:00', 664400000, 100000000);
+insert into traffics(pid, name, date, download, upload) values (89134, 'Netflix', '2022-12-02 10:00:00', 9876540000, 200000000);
+insert into traffics(pid, name, date, download, upload) values (8721234, 'Teams', '2022-12-03 10:00:00', 2200990000, 100000000);
+insert into traffics(pid, name, date, download, upload) values (86133, 'OneDrive', '2022-12-04 10:00:00', 2088770000, 200000000);
+
+insert into traffics(pid, name, date, download, upload) values (91213, 'Chrome', '2022-11-01 10:00:00', 66440000, 100000000);
+insert into traffics(pid, name, date, download, upload) values (97632, 'Netflix', '2022-11-02 10:00:00', 987654000, 200000000);
+insert into traffics(pid, name, date, download, upload) values (91563, 'OneDrive', '2022-11-04 10:00:00', 998877000, 200000000);
+insert into traffics(pid, name, date, download, upload) values (92456, 'Whatsapp', '2022-11-05 10:00:00', 12345609876, 100000000);
